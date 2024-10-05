b93425fbe3d3495e72887cb711429ac878b7d25e
AkashSinha007 <akashkumarsinha007@gmail.com>
2024-10-06 01:46:33 +0200

    Added Window Abstraction and GLFW

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -4,10 +4,13 @@
 #include "Hazel/Events/ApplicationEvent.h"
 #include "Hazel/Log.h"
 
+#include <GLFW/glfw3.h>
+
 namespace Hazel 
 {
     Application::Application()
     {
+        m_Window = std::unique_ptr<Window>(Window::Create());
     }
 
     Application::~Application()
@@ -16,18 +19,11 @@ namespace Hazel
 
     void Application::Run()
     {
-        WindowResizeEvent e(1280,720);
-
-        if(e.IsInCategory(EventCategoryApplication))
-        {
-            HZ_TRACE(e.ToString());
+        while (m_Running)
+		{
+            glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+			m_Window->OnUpdate();
         }
-
-        if(e.IsInCategory(EventCategoryInput))
-        {
-            HZ_TRACE(e.ToString());
-        }
-
-        // while(true);
     }
 }
